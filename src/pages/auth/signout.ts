420c3598568a977b034471364c02449d44d8f0c4
Mateusz Chodacki <mateusz.chodacki@gmail.com>
2025-07-28 16:02:15 +0200

    Build fix

--- src/pages/auth/signout.ts
+++
@@ -1,9 +0,0 @@
-export const prerender = false;
-import type { APIRoute } from 'astro';
-import { createSupabaseServerInstance } from '../../../lib/supabase';
-
-export const GET: APIRoute = async ({ cookies, headers, redirect }) => {
-  const supabase = createSupabaseServerInstance({ headers, cookies });
-  await supabase.auth.signOut();
-  return redirect('/auth/email');
-};
